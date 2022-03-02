c420823129f669c10e1a6f18d24d97a80d69e386
Basil Crow <me@basilcrow.com>
2022-03-02 09:50:20 -0800

    Improve test

--- src/test/java/org/glassfish/jersey/jackson/JacksonFeatureTest.java
+++ src/test/java/org/glassfish/jersey/jackson/JacksonFeatureTest.java
@@ -1,7 +1,10 @@
 package org.glassfish.jersey.jackson;
 
+import org.glassfish.jersey.client.JerseyClient;
+import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class JacksonFeatureTest {
@@ -10,6 +13,13 @@ public class JacksonFeatureTest {
 
     @Test
     public void smokes() throws Throwable {
-        rr.then(j -> new JacksonFeature());
+        rr.then(JacksonFeatureTest::_smokes);
+    }
+
+    private static void _smokes(JenkinsRule r) {
+        JerseyClientBuilder builder = new JerseyClientBuilder();
+        builder.register(JacksonFeature.class);
+        JerseyClient client = builder.build();
+        client.preInitialize();
     }
 }
